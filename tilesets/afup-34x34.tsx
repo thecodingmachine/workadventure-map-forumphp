69f401a9c032f41e5ed9c683da263c9f50246b49
Mathieu Lemoine <kura@kura.ooo>
2020-10-15 23:30:18 +0200

    🍱 New AFUP tiles (walls, carpets, floors, tables) + 🍱 banners for sponsors

---
+++ tilesets/afup-34x34.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.09.22" name="afup-34x34" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="afup-34x34.png" width="272" height="272"/>
+</tileset>
